da08315d4e3e071e3340c7d35a4d74080fbcd5ec
Kyle Anderson <kyle-l-anderson@uiowa.edu>
2017-04-03 19:26:09 -0500

    Payment.java methods completed

--- IowaAir/src/java/dbResources/Payment.java
+++ IowaAir/src/java/dbResources/Payment.java
@@ -10,7 +10,38 @@ package dbResources;
  * @author Kyle Anderson
  */
 public class Payment {
-    Integer cardNumber;
+    long cardNumber = -1;
+    int cvv = -1;
     
+    public Payment(String cardNumber, String cvv) {
+        this.cardNumber = validateCardNumber(cardNumber);
+        this.cvv = validateCVV(cvv);
+    }
+    
+    private static long validateCardNumber(String cardNumber) {
+        long validCard = -1;
+        if(cardNumber.length() != 10) {
+            return validCard;
+        }
+        try {
+            validCard = Long.parseLong(cardNumber);
+        } catch(NumberFormatException nfe) {
+            validCard = -1;
+            return validCard;
+        }
+        return validCard;
+    }
+    
+    private static int validateCVV(String cvv) {
+        int validCVV = -1;
+        if(cvv.length() != 3)
+        try {
+            validCVV = Integer.parseInt(cvv);
+        } catch(NumberFormatException nfe) {
+            validCVV = -1;
+            return validCVV;
+        }
+        return validCVV;
+    }
     
 }
