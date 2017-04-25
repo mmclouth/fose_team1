a86aef23f937350359cc38ee3041c0da71cf8bfa
mmclouth <mackenzie-mclouth@uiowa.edu>
2017-04-25 09:20:00 -0500

    Added overloaded method for send method in the SendMail class

--- IowaAir/src/java/dbResources/SendMail.java
+++ IowaAir/src/java/dbResources/SendMail.java
@@ -71,6 +71,43 @@ public class SendMail {
         return code;
     }
     
+    public void send(boolean isCustomer, String code) throws MessagingException {
+
+        // Get system properties object
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.user", from);
+        properties.put("mail.smtp.password", password);
+        properties.put("mail.smtp.port", 587);
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the default Session object.
+        Session mailSession = Session.getDefaultInstance(properties, null);
+
+        // Create a default MimeMessage object.
+        MimeMessage message = new MimeMessage(mailSession);
+        // Set From: header field of the header.
+        message.setFrom(new InternetAddress(from));
+        // Set To: header field of the header.
+        message.addRecipient(MimeMessage.RecipientType.TO,
+                                 new InternetAddress(to));
+        // Set Subject: header field
+        message.setSubject("Iowa Air Verification Code");
+        
+        if(isCustomer) {
+            message.setText("Your verification code is: " + code);
+        } else {
+            message.setText("Your initial password is: " + code);
+        }
+        //placement = "after setText";
+        // Send message
+        Transport transport = mailSession.getTransport("smtp");
+        transport.connect(host, from, password);
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+    }
+    
     //random string generation found at 
     //http://stackoverflow.com/questions/41107/how-to-generate-a-random-alpha-numeric-string
     public static String generateVerificationCode() {
